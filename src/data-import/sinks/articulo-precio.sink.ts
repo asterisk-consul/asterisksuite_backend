50357150ee1ed08658d88eb3b8cd2dab53725d26
MaxiPedano <maximiliano.pedano@gmail.com>
2026-03-10 08:12:11 -0300

    modificacion del sink

--- src/data-import/sinks/articulo-precio.sink.ts
+++ src/data-import/sinks/articulo-precio.sink.ts
@@ -2,26 +2,28 @@ import { Sink } from '../core/interfaces';
 import { PrismaService } from '../../prisma/prisma.service';
 
 export class ArticuloPrecioSink implements Sink<any> {
-  constructor(private prismaService: PrismaService) {}
+  constructor(private prisma: PrismaService) {}
 
   async send(data: any[]) {
-    const batchSize = 500;
+    const batchSize = 100;
 
     for (let i = 0; i < data.length; i += batchSize) {
       const batch = data.slice(i, i + batchSize);
 
-      const queries = batch.map((item) =>
-        this.prismaService.articuloprecio.updateMany({
-          where: {
-            articuloid: item.articuloid,
-          },
-          data: {
-            precio: item.precio,
-          },
-        }),
-      );
+      const ids = batch.map((item) => item.articuloid);
+      const precios = batch.map((item) => item.precio);
 
-      await this.prismaService.$transaction(queries);
+      const cases = batch
+        .map((item) => `WHEN ${item.articuloid} THEN ${Number(item.precio)}`)
+        .join(' ');
+
+      await this.prisma.$executeRawUnsafe(`
+        UPDATE test9000.articuloprecio
+        SET precio = CASE articuloid
+          ${cases}
+        END
+        WHERE articuloid IN (${ids.join(',')})
+      `);
     }
   }
 }
