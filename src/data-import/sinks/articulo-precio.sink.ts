5036923cae22c0d56866d4929b8b6ae59526f98b
Agustin <agustinyedro@gmail.com>
2026-07-13 11:37:44 -0300

    Fix trasacciones de prisma de 5s a 30s

--- src/data-import/sinks/articulo-precio.sink.ts
+++ src/data-import/sinks/articulo-precio.sink.ts
@@ -10,49 +10,35 @@ export class ArticuloPrecioSink implements Sink<any> {
     for (let i = 0; i < data.length; i += batchSize) {
       const batch = data.slice(i, i + batchSize);
 
-      const now = new Date(); // 👈 misma fecha para todo el batch
+      const now = new Date();
 
-      // 1. Update existentes
-      const updateQueries = batch.map((item) =>
-        this.prismaService.articuloprecio.updateMany({
-          where: { articuloid: item.articuloid },
-          data: {
+      await this.prismaService.$transaction(
+        async (tx) => {
+          for (const item of batch) {
+            await tx.articuloprecio.updateMany({
+              where: { articuloid: item.articuloid },
+              data: {
+                categid: 10790,
+                precio: item.precio,
+                changedate: now,
+              },
+            });
+          }
+
+          const createData = batch.map((item) => ({
+            articuloid: item.articuloid,
             categid: 10790,
             precio: item.precio,
-            changedate: now, // 👈 FIX
-          },
-        }),
+            changedate: now,
+          }));
+
+          await tx.articuloprecio.createMany({
+            data: createData,
+            skipDuplicates: true,
+          });
+        },
+        { timeout: 30000 },
       );
-
-      // 2. Insertar los que NO existen
-      const createData = batch.map((item) => ({
-        articuloid: item.articuloid,
-        categid: 10790,
-        precio: item.precio,
-        changedate: now, // 👈 FIX
-      }));
-
-      // // 3. Histórico
-      // const historicoData = batch.map((item) => ({
-      //   articuloid: item.articuloid,
-      //   categid: 10790, // 👈 opcional: consistencia
-      //   precio: item.precio,
-      //   changedate: now, // 👈 FIX consistente
-      //   factorconversion: 1,
-      // }));
-
-      await this.prismaService.$transaction([
-        ...updateQueries,
-
-        this.prismaService.articuloprecio.createMany({
-          data: createData,
-          skipDuplicates: true,
-        }),
-
-        // this.prismaService.articulopreciohistorico.createMany({
-        //   data: historicoData,
-        // }),
-      ]);
     }
   }
 }
